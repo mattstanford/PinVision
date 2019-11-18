d77518dbfe561acba80ab491b3f3b1ab1f780af3
Matt Stanford <mds6058@gmail.com>
2019-11-17 19:55:01 -0500

    Initial Commit

---
+++ PinVision/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PinVision
+//
+//  Created by Matt Stanford on 11/17/19.
+//  Copyright © 2019 Matt Stanford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
